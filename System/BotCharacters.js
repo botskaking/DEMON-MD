e80a1387067339246bdd7bd4b67d2ba21cc93270
botskaking <152233761+botskaking@users.noreply.github.com>
2024-02-11 12:07:13 +0530

    Update BotCharacters.js

--- System/BotCharacters.js
+++ System/BotCharacters.js
@@ -280,18 +280,6 @@ global.charID18 = {
 }
 
 global.charID19 = {
-  botName : "Eiko",
-  botVideo : "https://graph.org/file/7f99357fec0f000c7ab0a.mp4",
-  botImage1 : "https://images8.alphacoders.com/122/1229829.jpg",
-  botImage2 : "https://images2.alphacoders.com/123/1234944.png",
-  botImage3 : "https://images5.alphacoders.com/123/1235274.jpg",
-  botImage4 : "https://images6.alphacoders.com/126/1260043.png",
-  botImage5 : "https://images6.alphacoders.com/124/1247977.jpg",
-  botImage6 : "https://images2.alphacoders.com/125/1251795.jpg",
-}
-
-
-global.charID20 = {
   botName : "Shizuka MD",
   botVideo : "https://raw.githubusercontent.com/salmanytofficial/XLICON-MD/main/lib/assets/help.mp4",
   botImage1 : "https://iili.io/J1eiev2.jpg",
